e71f74f16f98d970314805899880f680478eca23
pvsameerpvs <pvsameerpvs@gmail.com>
2026-05-23 18:32:15 +0400

    fix: userfrontend port change

--- apps/customer-frontend/next.config.js
+++ apps/customer-frontend/next.config.js
@@ -1,9 +1,6 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
@@ -11,14 +8,6 @@ const nextConfig = {
       { protocol: 'http', hostname: '**' },
     ],
   },
-  allowedDevOrigins: [
-    'localhost:3000',
-    '192.168.70.30',
-    '*.loca.lt',
-    '*.ngrok-free.app',
-    '*.ngrok.io',
-    '0.0.0.0'
-  ],
 }
 
 module.exports = nextConfig
